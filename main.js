fe69b9b2bafb9393973a006957cc675b4a7dcf9e
DixonKH <khaydarovdilshod9@gmail.com>
2025-03-27 13:32:03 +0900

    feat: merge TwoLists

--- main.js
+++ main.js
@@ -1,58 +1,34 @@
 // nodemon main.js    to run the file
 
-//          Valid Parentheses
-
-// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-// An input string is valid if:
-
-// Open brackets must be closed by the same type of brackets.
-// Open brackets must be closed in the correct order.
-// Every close bracket has a corresponding open bracket of the same type.
-
- 
-
-// Example 1:
-
-// Input: s = "()"
-// Output: true
-// Example 2:
-
-// Input: s = "()[]{}"    
-// Output: true
-// Example 3:     
-
-// Input: s = "(]"                
-// Output: false    
-
-// Solution:
-
-  function isValid(s) {
-    let stack = [];
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
-        stack.push(s[i]); // add the element to the end of the array
-      } else {
-        if (stack.length === 0) return false;
-        if (
-          (s[i] === ")" && stack[stack.length - 1] === "(") ||
-          (s[i] === "]" && stack[stack.length - 1] === "[") ||
-          (s[i] === "}" && stack[stack.length - 1] === "{")
-        ) {
-          stack.pop(); // remove the last element from the array
-        } else {
-          return false;
-        }
+var mergeTwoLists = function(list1, list2) {
+  if(list1.length === 0) return list2;
+  if(list2.length === 0) return list1;
+  const merge = [];
+  let i = 0, j = 0;
+  while(i < list1.length && j < list2.length) { // O(n) + O(m) time complexity,  O(n) + O(m) space complexity
+      if(list1[i] < list2[j]){
+          merge.push(list1[i]);
+          i++;
+      }else {
+          merge.push(list2[j]);
+          j++;
       }
-    }
-    return stack.length === 0;
   }
 
- // console.log("case1: ", isValid("()"));
-  console.log("case2: ", isValid("([])"));
-  //console.log("case3: ", isValid("()[]{}"));
-  //console.log("case3: ", isValid("(]")); 
+  while(i < list1.length) {
+      merge.push(list1[i]);
+      i++;
+  }
 
+  while(j < list2.length) {
+      merge.push(list2[j]);
+      j++;
+  }
+return merge;  
+};
+
+console.log(mergeTwoLists([1,2,4], [1,3,4]));
+ // [1,1,2,3,4,4]
 
  
    
